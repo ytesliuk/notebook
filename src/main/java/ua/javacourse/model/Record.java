f73ceaee8e8b4beaf3c39a4c9a1b8920a561b0aa
Yuliia Tesliuk <ytesliuk@gmail.com>
2018-10-28 00:57:38 +0300

    change Record.class implementation by providing EnumMap as main field which gather all the required record information

--- src/main/java/ua/javacourse/model/Record.java
+++ src/main/java/ua/javacourse/model/Record.java
@@ -6,138 +6,59 @@ import java.util.*;
  * Created by Yuliia Tesliuk on 10/23/2018
  */
 public class Record implements Comparable {
-    private String lastName;
-    private String firstName;
-    private String middleName;
-    private String nickname;
-    private Group group;
-    private List phoneNumbers;
-    private String email;
-    private String skype;
+    private String name;
     private Address address;
-    private String comment;
+    private Map<RecFields, String> record;
     private Date createDate;
     private Date changeDate;
 
-    public Record (Map<RecordFieldsEnum,String> record) {
-        this.lastName = record.get(RecordFieldsEnum.LAST_NAME);
-        this.firstName = record.get(RecordFieldsEnum.FIRST_NAME);
-        this.middleName = record.get(RecordFieldsEnum.MIDDLE_NAME);
-        this.nickname = record.get(RecordFieldsEnum.NICKNAME);
-        this.group = Group.getGroup(record.get(RecordFieldsEnum.GROUP));
-        this.phoneNumbers = new ArrayList<String>();
-        phoneNumbers.add(record.get(RecordFieldsEnum.HOME_PHONE_NUMBER));
-        phoneNumbers.add(record.get(RecordFieldsEnum.MOBILE_PHONE_NUMBER));
-        this.email = record.get(RecordFieldsEnum.EMAIL);
-        this.skype = record.get(RecordFieldsEnum.SKYPE);
-        this.address = new Address( record.get(RecordFieldsEnum.INDEX),
-                                    record.get(RecordFieldsEnum.CITY),
-                                    record.get(RecordFieldsEnum.STREET),
-                                    record.get(RecordFieldsEnum.BUILDING),
-                                    record.get(RecordFieldsEnum.APT));
-        this.comment = record.get(RecordFieldsEnum.COMMENT);
+    public Record(Map<RecFields, String> record) {
+        this.name = record.get(RecFields.LAST_NAME) + " " + record.get(RecFields.FIRST_NAME) + " "  + record.get(RecFields.MIDDLE_NAME);
+        this.address = new Address(record.get(RecFields.INDEX), record.get(RecFields.CITY), record.get(RecFields.STREET), record.get(RecFields.BUILDING), record.get(RecFields.APT));
+        this.record = record;
         createDate = new Date();
+
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getFieldValue(RecFields field) {
+        return record.get(field);
     }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-        this.changeDate = new Date();
+
+    public void setFieldValue(RecFields field, String value) {
+        record.put(field, value);
+        changeDate = new Date();
     }
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getMiddleName() {
-        return middleName;
-    }
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-    public String getNickname() {
-        return nickname;
-    }
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-    public String getComment() {
-        return comment;
-    }
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-    public Group getGroup() {
-        return group;
-    }
-    public void setGroup(Group group) {
-        this.group = group;
-    }
-    public List getPhoneNumbers() {
-        return phoneNumbers;
-    }
-    public void setPhoneNumbers(List phoneNumbers) {
-        this.phoneNumbers = phoneNumbers;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getSkype() {
-        return skype;
-    }
-    public void setSkype(String skype) {
-        this.skype = skype;
-    }
-    public Address getAddress() {
-        return address;
-    }
-    public void setAddress(Address address) {
-        this.address = address;
-    }
+
     public Date getCreateDate() {
         return createDate;
     }
     public Date getChangeDate() {
         return changeDate;
     }
-    private void setChangeDate(Date changeDate) {
-        this.changeDate = changeDate;
-    }
 
     @Override
     public boolean equals(Object obj) {
         if (obj == null) return false;
         if (obj instanceof  Record) {
-            Record otherRecord = (Record) obj;
-            if (this.lastName.equals(((Record) obj).lastName)
-            && this.firstName.equals(((Record) obj).firstName)
-            && this.middleName.equals(((Record) obj).middleName)
-            && this.nickname.equals(((Record) obj).nickname));
-        }
-        return super.equals(obj);
+            Record otherRec = (Record) obj;
+            return this.name.equals(otherRec.name);
+        } else return false;
     }
 
     @Override
     public int compareTo(Object o) {
         Record rec = (Record) o;
-        return (this.lastName + this.firstName + this.middleName).compareTo(rec.lastName + rec.firstName + rec.middleName);
+        return (this.name).compareTo(rec.name);
     }
 
     @Override
     public String toString() {
-        StringBuilder record = new StringBuilder();
-        record.append(lastName).append(" ").append(firstName).append(" ").append(middleName).append(" ")
-                .append(nickname.isEmpty() ? "" : "(" + nickname + ")").append(" в группе ").append(group.getRussianName())
-                .append("\n").append("номера телефонов: ").append(Arrays.toString(phoneNumbers.toArray()))
-                .append("\n").append("email: ").append(email).append(" / skype: ").append(skype)
-                .append("\n").append("адресс: ").append(address.toString())
-                .append("\n").append("комментарий: ").append(comment);
-        return record.toString();
+        StringBuilder recordInfo = new StringBuilder();
+        recordInfo.append(name).append(" ").append(record.get(RecFields.NICKNAME).isEmpty() ? "" : "(" + record.get(RecFields.NICKNAME) + ")")
+                .append(" в группе ").append(record.get(RecFields.GROUP))
+                .append("\n").append("номера телефонов: ").append(record.get(RecFields.HOME_PHONE_NUMBER) + ", ").append(record.get(RecFields.MOBILE_PHONE_NUMBER))
+                .append("\n").append("email: ").append(record.get(RecFields.EMAIL)).append(" / skype: ").append(record.get(RecFields.SKYPE))
+                .append("\n").append("адресс: ").append(address.toString()).append("\n").append("комментарий: ").append(record.get(RecFields.COMMENT));
+        return recordInfo.toString();
     }
 }
